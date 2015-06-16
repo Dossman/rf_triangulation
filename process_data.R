a3f1cb441968b342bc49760b8d71ae0e17d30b99
Dossman <bdossman@gmail.com>
2015-06-16 12:30:43 -0400

    changed name
    changed name

--- process_data.R
+++ process_data.R
@@ -1,10 +1,10 @@
-## raw2process.r : code takes raw data from receivers and processes to for using in the 
+## process_data.r : code takes raw data from receivers and processes to for using in the 
 ## bearing_estimator function. This code gets applied across time steps (t) and, for each tower
 ## that detects the tag, will pull out the strongest recieved signal strengths from two
 ## neighboring antennas. Output will consist of t (timestep), towers (for which a tag was 
 ## detected), antenna bearing with max signal strength, signal strength for that antenna, 
 ## antenna bearing with next strongest singal, and signal strength for that antenna.
 
-raw2process <- function(df=data){
+process_data <- function(df=data){
 	
 }
